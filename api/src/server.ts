bd7a86b0a8ba75fd92957b7841caa3966901722d
Mateus Mourão <mateusm@take.net>
2020-08-09 17:17:09 -0300

    add db connection

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,1 +1,16 @@
-console.log('Hello World');
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+app.get('/users', (req, res) => {
+  const users = [
+    {
+      name: "Mateus Mourao",
+      age: "22"
+    }
+  ];
+  return res.json(users);
+});
+
+app.listen(3333);
